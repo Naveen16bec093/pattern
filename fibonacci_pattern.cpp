a6db4bb2ccde0ca9724dd31567da593e5f330963
Naveen16bec093 <56761785+Naveen16bec093@users.noreply.github.com>
2020-08-16 16:26:07 +0530

    Update fibonacci_pattern.cpp

--- fibonacci_pattern.cpp
+++ fibonacci_pattern.cpp
@@ -2,12 +2,55 @@
 
 using namespace std;//Namespace for using cin,cout..etc.
 
-int main(){
 
-int i,num,a,b,temp;//Variable Declaration
 
-cout<<"Enter the Number:";
-cin>>num;
+
+void print_asterisk(int asterisk) 
+{ 
+    if (asterisk == 0) 
+        return; 
+    cout << "* "; 
+    print_asterisk(asterisk - 1); 
+} 
+  
+// function to print the upper-half of the pattern 
+void pattern_upper(int n, int num) 
+{ 
+    // base case 
+    if (n == 0) 
+        return; 
+    print_asterisk(n); 
+    cout << endl; 
+  
+    // recursively calling pattern_upper() 
+    pattern_upper(n - 1, num); 
+} 
+  
+// function to print the lower-half of the pattern 
+void pattern_lower(int n, int num) 
+{ 
+    // base case 
+    if (n == 0) 
+        return; 
+    print_asterisk(num - n + 1); 
+    cout << endl; 
+  
+    // recursively calling pattern_lower() 
+    pattern_lower(n - 1, num); 
+} 
+  
+// function to print the pattern 
+void pattern(int n, int num) 
+{ 
+    pattern_upper(n, num); 
+    pattern_lower(n - 1, num); 
+} 
+
+void fib(int num)
+{
+	
+int i,a,b,temp;//Variable Declaration
+
 
 for(int x=1;x<num;x++){              //Iterating for the number of lines
 	for(i=1,a=0,b=1;i<=x;++i){   //Iterating for the fibannoci series
@@ -16,6 +59,19 @@ for(int x=1;x<num;x++){              //Iterating for the number of lines
 		}
 		cout<<"\n";
     }
+}
 
+int main(){
+	
+int num;
+cout<<"Enter the Number:";
+cin>>num;
+		fib(num);
+    
+int n = 5; 
+  		pattern(n, n);
+	
 return 0;
 }
+
+
