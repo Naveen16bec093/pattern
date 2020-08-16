35d02dc4e6aa9940b1805f88851925e217872db8
Naveen16bec093 <56761785+Naveen16bec093@users.noreply.github.com>
2020-08-16 16:08:13 +0530

    Added Comments

--- fibonacci_pattern.cpp
+++ fibonacci_pattern.cpp
@@ -6,13 +6,13 @@ int main(){
 
 int i,num,a,b,temp;
 
-cout<<"Enter:";
+cout<<"Enter the Number:";
 cin>>num;
 
-for(int x=1;x<num;x++){
-	for(i=1,a=0,b=1;i<=x;++i){
+for(int x=1;x<num;x++){              //Iterating for the number of lines
+	for(i=1,a=0,b=1;i<=x;++i){   //Iterating for the fibannoci series
 		cout<<b<<"  ";
-		temp=a+b;a=b;b=temp;
+		temp=a+b;a=b;b=temp; //Upating the values
 		}
 		cout<<"\n";
     }
